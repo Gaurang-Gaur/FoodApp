61f90b3691787c45eb9cd084f619d72a80af46e9
Gaurang <gauranggaur007@gmail.com>
2024-01-04 16:05:42 +0530

    using useRouteEffect hook

--- src/Component/Error.js
+++ src/Component/Error.js
@@ -1,4 +1,11 @@
+import {useRouteError} from "react-router-dom";
+
+
+
 const Error=()=>{
+    const err=useRouteError();
+
+    console.log(err);
 
 
 
@@ -6,6 +13,7 @@ const Error=()=>{
         <div>
             <h1> OOps something wrong </h1>
             <h2> checking our side for error</h2>
+            <h4>{err.statusText}</h4>
         </div>
     )
 }
