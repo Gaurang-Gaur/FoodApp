be77b4880b78fc6e2fdad010f50002dd37fd428a
Gaurang <gauranggaur007@gmail.com>
2024-01-08 22:27:25 +0530

    adding the restaurant data

--- src/Component/RestaurantMenu.js
+++ src/Component/RestaurantMenu.js
@@ -1,6 +1,24 @@
+
+import {useEffect, useState} from "react";
+
 const RestaurantMenu=()=>{
 
 
+    useEffect (()=>{
+
+        fetchApi();
+
+    },[]);
+
+
+    const fetchApi=async()=>{
+
+
+        const ApiData=await fetch("https://gaurang-gaur.github.io/host_api/apiData.json");
+
+        const data= await ApiData.json();
+        console.log(data);
+    }
 
 
 
